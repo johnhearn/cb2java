656a4d81f4229752a115186e20e2b685db783221
Dev Stop Fix <james@devstopfix.net>
2012-06-30 13:12:27 +0100

    Copybook parser simple test case

---
+++ src/test/java/net/sf/cb2java/copybook/CopybookParserTest.java
@@ -0,0 +1,29 @@
+package net.sf.cb2java.copybook;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import junit.framework.TestCase;
+import net.sf.cb2java.copybook.Copybook;
+import net.sf.cb2java.copybook.CopybookParser;
+
+/**
+ *
+ * @author devstopfix
+ */
+public class CopybookParserTest extends TestCase {
+    
+    public CopybookParserTest(String testName) {
+        super(testName);
+    }
+
+    /**
+     * Take a copybook and parse it.
+     *
+     * @throws FileNotFoundException
+     */
+    public void testWeCanParseCopybook() throws FileNotFoundException {
+        Copybook copybook = CopybookParser.parse("A", new FileInputStream(new File("./target/test-classes/a.copybook")));
+        assertEquals(55, copybook.getLength());
+    }
+}
