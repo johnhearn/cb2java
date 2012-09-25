9ffc01f6fb2b119c97878f09b8ecb01d2d6cb663
devstopfix <james@devstopfix.net>
2012-09-25 11:08:18 +0100

    Add simple test case for parsing copybook data, numerics not yet tested

--- src/test/java/net/sf/cb2java/copybook/CopybookParserTest.java
+++ src/test/java/net/sf/cb2java/copybook/CopybookParserTest.java
@@ -6,6 +6,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.List;
 import junit.framework.TestCase;
+import net.sf.cb2java.data.Data;
 import net.sf.cb2java.data.Record;
 
 /**
@@ -38,5 +39,12 @@ public class CopybookParserTest extends TestCase {
         assertEquals(31, copybook.getLength());
         List<Record> results = copybook.parseData(new FileInputStream(new File("./target/test-classes/b.input.txt")));
         assertEquals(1, results.size());
+        Record record = results.get(0);
+        Data root = record.getChild("ROOT");
+        assertEquals("ABCDEF", root.getChildren().get(0).toString());
+        assertEquals("B", ((Data)root.getChildren().get(1)).getName().toString());
+        assertEquals("BCDE", root.getChildren().get(1).toString());
+        // TODO assertEquals(12345, root.getChildren().get(2).toString());
+        // TODO assertEquals(1234, ((Data)root.getChildren().get(3)).getValue());
     }
 }
