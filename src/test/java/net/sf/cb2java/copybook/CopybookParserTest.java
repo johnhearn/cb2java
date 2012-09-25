0fe48bb4909fba202c274f75338891d730bb0471
devstopfix <james@devstopfix.net>
2012-09-25 10:50:50 +0100

    Add simple test case for parsing copybook data

--- src/test/java/net/sf/cb2java/copybook/CopybookParserTest.java
+++ src/test/java/net/sf/cb2java/copybook/CopybookParserTest.java
@@ -3,6 +3,7 @@ package net.sf.cb2java.copybook;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import junit.framework.TestCase;
 
 /**
@@ -23,5 +24,16 @@ public class CopybookParserTest extends TestCase {
     public void testWeCanParseCopybook() throws FileNotFoundException {
         Copybook copybook = CopybookParser.parse("A", new FileInputStream(new File("./target/test-classes/a.copybook")));
         assertEquals(55, copybook.getLength());
+    }
+    
+    /**
+     * Parse copybook data.
+     *
+     * @throws FileNotFoundException
+     */
+    public void testWeCanParseCopybookData() throws FileNotFoundException, IOException {
+        Copybook copybook = CopybookParser.parse("B", new FileInputStream(new File("./target/test-classes/b.copybook")));
+        assertEquals(31, copybook.getLength());
+        copybook.parseData(new FileInputStream(new File("./target/test-classes/b.input.txt")));
     }
 }
