a97c57a2efc1a439cbafa006e33097f7f4cecc0f
John Hearn <hearn.john@gmail.com>
2014-04-11 14:13:35 +0200

    Fix test (encoding not set)

--- src/test/java/net/sf/cb2java/types/AlphaNumericTest.java
+++ src/test/java/net/sf/cb2java/types/AlphaNumericTest.java
@@ -1,5 +1,7 @@
 package net.sf.cb2java.types;
 
+import net.sf.cb2java.Settings;
+import net.sf.cb2java.Values;
 import junit.framework.TestCase;
 
 public class AlphaNumericTest extends TestCase {
@@ -9,13 +11,13 @@ public class AlphaNumericTest extends TestCase {
 	}
 
 	public void testValidateX2000() {
-		AlphaNumeric cut = new AlphaNumeric("DATA-ITEM", 5, 0, "X(100)");
+		AlphaNumeric cut = createClassUnderTest();
 		cut.validate("Lorem ipsum dolor sit amet");
 	}
 
 	public void testValidateX2000WithCarriageReturn() {
 		try {
-			AlphaNumeric cut = new AlphaNumeric("DATA-ITEM", 5, 0, "X(100)");
+			AlphaNumeric cut = createClassUnderTest();
 			cut.validate("Lorem ipsum \n dolor sit amet");
 			fail("AlphaNumeric does not currently allow carriage return characters.");
 		} catch (IllegalArgumentException e) {
@@ -24,6 +26,15 @@ public class AlphaNumericTest extends TestCase {
 			// from the character set of the computer."
 			// http://publib.boulder.ibm.com/infocenter/ratdevz/v7r1m1/topic/com.ibm.ent.cbl.zos.doc/topics/igy3lr31.pdf
 		}
+	}
+
+	protected AlphaNumeric createClassUnderTest() {
+		AlphaNumeric cut = new AlphaNumeric("DATA-ITEM", 5, 0, "X(100)");
+		
+		Values values = Settings.DEFAULT.getValues();
+		values.setEncoding(Settings.DEFAULT.getEncoding());
+		cut.setValue(values.SPACES);
+		return cut;
 	}
 
 }
