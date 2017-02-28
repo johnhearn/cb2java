56d2ac84bfffbae5d754a63b36d81c283b302d4f
Bert Koorengevel <bert.koorengevel@ing.nl>
2017-02-28 16:32:31 +0100

    avoid IOException by using a try-with-resources construction

--- src/main/java/net/sf/cb2java/copybook/CobolPreprocessor.java
+++ src/main/java/net/sf/cb2java/copybook/CobolPreprocessor.java
@@ -24,62 +24,54 @@ import java.io.Reader;
 import java.util.Properties;
 
 /**
- * Very simple COBOL pre-processor that chops the left and right margins. 
- * Column start and end positions are configurable using a properties file. 
- * Linefeeds are retained as these are required by the main parser.
- * COBOL files typically contain some junk characters and 
- * comment indicators in the "margins" and this routine removes those. 
+ * Very simple COBOL pre-processor that chops the left and right margins. Column
+ * start and end positions are configurable using a properties file. Linefeeds
+ * are retained as these are required by the main parser. COBOL files typically
+ * contain some junk characters and comment indicators in the "margins" and this
+ * routine removes those.
  * 
  * @author Peter Thomas
  */
 public class CobolPreprocessor {
 
-      public static String preProcess(Reader reader) {
-        int columnStart = 6;
-        int columnEnd = 72;
-        
-        try {
-//            FileInputStream propsStream = new FileInputStream("cb2xml.properties");
-            Properties props = new Properties();
-//            props.load(propsStream);
-//            propsStream.close();
-            String columnStartProperty = props.getProperty("column.start");
-            String columnEndProperty = props.getProperty("column.end");
-            if (columnStartProperty != null) {
-                columnStart = Integer.parseInt(columnStartProperty);
-            }
-            if (columnEndProperty != null) {
-                columnEnd = Integer.parseInt(columnEndProperty);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+	public static String preProcess(Reader reader) {
+		int columnStart = 6;
+		int columnEnd = 72;
 
-        StringBuffer sb = new StringBuffer();
+		Properties props = new Properties();
+		String columnStartProperty = props.getProperty("column.start");
+		String columnEndProperty = props.getProperty("column.end");
+		if (columnStartProperty != null) {
+			columnStart = Integer.parseInt(columnStartProperty);
+		}
+		if (columnEndProperty != null) {
+			columnEnd = Integer.parseInt(columnEndProperty);
+		}
 
-        try (BufferedReader buffer = new BufferedReader(reader)) {
-          String s = null;
-          while ((s = buffer.readLine()) != null) {         
-            if (s.length() > columnStart) {
-                int thisColumnStart = columnStart;
-                if (s.charAt(columnStart) == '/') {
-                    sb.append('*');
-                    thisColumnStart++;
-                }               
-                if (s.length() < columnEnd) {
-                    sb.append(s.substring(thisColumnStart));
-                } else {
-                    sb.append(s.substring(thisColumnStart, columnEnd));
-                }   
-            }
-            sb.append("\n");
-          }
-        }
-        catch (IOException e) {
-          e.printStackTrace();
-          return null;
-        }
-        
-        return sb.toString();
-      }
+		StringBuffer sb = new StringBuffer();
+
+		try (BufferedReader buffer = new BufferedReader(reader)) {
+			String s = null;
+			while ((s = buffer.readLine()) != null) {
+				if (s.length() > columnStart) {
+					int thisColumnStart = columnStart;
+					if (s.charAt(columnStart) == '/') {
+						sb.append('*');
+						thisColumnStart++;
+					}
+					if (s.length() < columnEnd) {
+						sb.append(s.substring(thisColumnStart));
+					} else {
+						sb.append(s.substring(thisColumnStart, columnEnd));
+					}
+				}
+				sb.append("\n");
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		}
+
+		return sb.toString();
+	}
 }
